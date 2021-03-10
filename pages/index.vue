905e77a526a12af76856759d1e4b278bba8c6192
Crybaka <kryptolone@protonmail.com>
2021-03-10 18:22:32 +0200

    Switch and checkbox sketch, other changes (HTML, SASS)

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,15 @@
       <label class="ol-field__title">Comment<span class="ol-field__counter">0/180</span></label>
     </fieldset>
 
+    <div class="ol">
+      <button class="ol-checkbox ol-checkbox--checked"></button>
+      <label class="ol-checkbox__title">check me!</label>
+    </div>
+
+    <div class="ol">
+      <button class="ol-switch ol-switch--active"></button>
+      <label class="ol-checkbox__title">switch me!</label>
+    </div>
+
   </main>
 </template>
