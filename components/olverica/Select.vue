de89b84559345332f5d71708af41f9b3c527edb5
x4e554c4c <wanderingdwarf03@gmail.com>
2021-03-15 21:54:24 +0200

    selected fix

--- components/olverica/Select.vue
+++ components/olverica/Select.vue
@@ -1,7 +1,9 @@
 <template>
   <fieldset 
     class="ol-field-defaults ol-field ol-field--select"
-    :class="{'ol-field--active': active}"
+    :class="{'ol-field--active': active,
+             'ol-field--error': failed,
+             'ol-field--focused': active}"
     v-click-outside="hide"
     @click="toggle">
 
@@ -12,9 +14,13 @@
       @mouseover="focus"
       @mouseleave="blur">
 
-      <span class="ol-field__select_placeholder">
+      <span 
+        class="ol-field__select_placeholder"
+        @click="select(-1)">
+
         select your option
       </span>
+
       <span 
         v-for="({label}, index) of this.map"
         :key="index"
@@ -31,13 +37,13 @@
 </template>
 
 <script lang="ts">
-import {Field, Form, isForm} from '~/services/types' 
+import {Field, Form, isForm, Validatable} from '~/services/types' 
 import {Component, Inject, Prop} from 'vue-property-decorator'
 import Vue from 'vue'
 
 
 @Component
-export default class OlvericaSelect extends Vue implements Field {
+export default class OlvericaSelect extends Vue implements Field, Validatable {
 
   @Inject()
   readonly $form!: Form|null;
@@ -45,24 +51,37 @@ export default class OlvericaSelect extends Vue implements Field {
   @Prop({type: String, default: 'select'})
   readonly name!: string;
 
+  @Prop({type: String, default: 'Select one option'})
+  readonly title!: string;
+
   @Prop({type: Array, default: () => ([])} )
   readonly map!: {label: string, value: unknown}[]
 
-  @Prop({type: Number, default: 0})
+  @Prop({type: Number, default: -1})
   readonly default!: number;
 
-  private selected = 0;
+  private selected = -1;
   
   private focused = false;
 
   private active = false;
+
+  private validated = false;
 
   get form(): Form|null {
     let form = this.$form;
     return isForm(form) ? form : null;
   }
 
+  get failed(): boolean {
+    return this.validated && 
+      (this.selected <= -1 || this.selected >= this.map.length);
+  }
+
   get label(): string {
+    if (this.selected <= -1 || this.selected >= this.map.length)
+      return this.title;
+
     let item = this.map[this.selected];
     return item.label
   }
@@ -77,7 +96,7 @@ export default class OlvericaSelect extends Vue implements Field {
   }
   
   select(index: number) {
-    this.selected = index
+    this.selected = index;
   }
 
   toggle(): void {
@@ -94,6 +113,11 @@ export default class OlvericaSelect extends Vue implements Field {
 
   blur() {
     this.focused = false;
+  }
+
+  validate(): boolean {
+    this.validated = true;
+    return !!!this.failed;
   }
 
   register() {
